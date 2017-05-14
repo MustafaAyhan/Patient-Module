458c19bc9e0d621c86bd50cdfa5d39048d68d362
AykutToprak <aykutoprak2315@gmail.com>
2017-05-14 17:48:47 +0300

    patient Module
    This is patient Module

---
+++ admin/includes/admin_footer.php
@@ -0,0 +1,7 @@
+        </div>
+        <!-- jQuery -->
+        <script src="/deu-cure/admin/js/scripts.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
