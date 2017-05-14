b1ff711a55d05f0960671627194eeafd6a562d07
AykutToprak <aykutoprak2315@gmail.com>
2017-05-14 18:01:59 +0300

    Revert "patient Module"
    This reverts commit 458c19bc9e0d621c86bd50cdfa5d39048d68d362.

--- admin/includes/admin_footer.php
+++
@@ -1,7 +0,0 @@
-        </div>
-        <!-- jQuery -->
-        <script src="/deu-cure/admin/js/scripts.js"></script>
-        <!-- Bootstrap Core JavaScript -->
-        <script src="js/bootstrap.min.js"></script>
-    </body>
-</html>
--- emergency.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-// read POST variables
-//$user = array();
-//array_push( $user, array("Tc"=>$_GET['tc'], "Name"=>$_GET['firstName'], "SurName"=>$_GET['surName'], "Address" => $_GET['address']));
-
-//header('Content-type: application/json');
-echo $_GET['Emergency'];
-?>
